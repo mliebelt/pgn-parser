f2177fda05f9c14ae89070e7170a30b9cebb4ecf
Michael Hobbs <michael.lee.hobbs@gmail.com>
2021-06-09 21:04:32 -0400

    simplified build script and remove shelljs requirement

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
-const path = require('path')
-const shell = require('shelljs')
+const peg = require('peggy')
+const fs = require('fs/promises')
 
-const peggyPath = path.resolve('./node_modules/peggy/bin/peggy')
 // node_modules/peggy/bin/peggy --allowed-start-rules pgn,tags,game,games -o _pgn-parser.js src/pgn-rules.pegjs
-shell.exec(`node ${peggyPath} --allowed-start-rules pgn,tags,game,games -o _pgn-parser.js src/pgn-rules.pegjs`)
+const main = async () => {
+    const options = {allowedStartRules: ['pgn', 'tags', 'game', 'games'], output: 'source', format: 'umd'}
+    const pgnParser = peg.generate(await fs.readFile('./src/pgn-rules.pegjs', 'utf-8'), options)
+    await fs.writeFile('_pgn-parser.js', pgnParser)
+}
+main().catch(console.error)
