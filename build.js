fe9fea1f5ef28f815dcf8bd14c3d25de3211f391
Michael Hobbs <michael.lee.hobbs@gmail.com>
2021-06-09 21:12:46 -0400

    switched out fs/promises for plain fs and used read/write sync to support node 12 in build script

--- build.js
+++ build.js
@@ -1,10 +1,10 @@
 const peg = require('peggy')
-const fs = require('fs/promises')
+const fs = require('fs')
 
 // node_modules/peggy/bin/peggy --allowed-start-rules pgn,tags,game,games -o _pgn-parser.js src/pgn-rules.pegjs
 const main = async () => {
     const options = {allowedStartRules: ['pgn', 'tags', 'game', 'games'], output: 'source', format: 'umd'}
-    const pgnParser = peg.generate(await fs.readFile('./src/pgn-rules.pegjs', 'utf-8'), options)
-    await fs.writeFile('_pgn-parser.js', pgnParser)
+    const pgnParser = peg.generate(await fs.readFileSync('./src/pgn-rules.pegjs', 'utf-8'), options)
+    await fs.writeFileSync('_pgn-parser.js', pgnParser)
 }
 main().catch(console.error)
