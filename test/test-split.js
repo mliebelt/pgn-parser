b5056c2f2b4bf5cd5becd3cd50ff734b3e33ac10
Markus Liebelt <mliebelt@hey.com>
2021-06-15 16:52:05 +0200

    #74 Adding examples how to use split

--- test/test-split.js
+++ test/test-split.js
@@ -1,10 +1,11 @@
-let parser = require("../split-games.js");
+let splitter = require("../split-games.js")
+let parser = require("../pgn-parser")
 
-let should = require('should');
-let fs = require('fs');
+let should = require('should')
+let fs = require('fs')
 
 function split_games(string) {
-    return parser.split(string, {startRule: "games"})
+    return splitter.split(string, {startRule: "games"})
 }
 
 describe("Should able to split one game", function () {
@@ -26,6 +27,36 @@ describe("When reading many games and split them", function () {
             if (err) { throw err }
             let res = split_games(data)
             should(res.length).equal(52966)
+            done()
+        })
+    })
+    it("be able to filter games depending on tags", function (done) {
+        fs.readFile(process.cwd() + '/test/pgn/32games.pgn', 'utf8', function (err, data) {
+            if (err) { throw err }
+            let res = split_games(data)
+            let found = []
+            res.forEach(function (game) {
+                let tags = parser.parse(game.tags, {startRule: "tags"})
+                if (tags.Result == "1-0") {
+                    found.push(parser.parse(game.all, {startRule: "game"}))
+                }
+            })
+            should(found.length).equal(22)
+            done()
+        })
+    })
+    it("should be able to print out players", function (done) {
+        fs.readFile(process.cwd() + '/test/pgn/32games.pgn', 'utf8', function (err, data) {
+            if (err) { throw err }
+            let res = split_games(data)
+            let players = []
+            res.forEach(function (game) {
+                let tags = parser.parse(game.tags, {startRule: 'tags'})
+                players.push(tags.White)
+                players.push(tags.Black)
+            })
+            let unique = [...new Set(players)]
+            should(unique.length).equal(38)
             done()
         })
     })
