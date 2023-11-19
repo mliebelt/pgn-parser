62b324f78154c88402754253702f0fe8935624ff
Markus Liebelt <mliebelt@hey.com>
2023-11-19 19:48:32 +0100

    #79 Export function parseFiles as utility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { parse, parseGames, parseGame, SyntaxError } from './pgn-parser'
 export { split, SplitGame } from './split-games'
-export { filesToJson } from './pgn-to-json'
+export { parseFiles } from './pgn-to-json'
 export * from './types'
--- src/pgn-to-json.ts
+++ src/pgn-to-json.ts
@@ -1,25 +1,24 @@
-import {ParseTree} from "./types";
-import { parse} from "./pgn-parser";
+import {ParseTree} from "./types"
+import { parseGames} from "./pgn-parser"
 
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs")
+const path = require("path")
 
-export function filesToJson(files: string[])  {
-    const games: ParseTree[][] = [];
+export function parseFiles(files: string[])  {
+    const games: ParseTree[][] = []
 
     for (const file of files) {
         const fileContent = fs
             .readFileSync(path.resolve(file))
             .toString()
-            .trim();
+            .trim()
 
         if (fileContent) {
-            const gamesOnFile = parse(fileContent, { startRule: "games" }) as ParseTree[];
-            // @ts-ignore
-            games.push(gamesOnFile);
+            const gamesOnFile = parseGames(fileContent) as ParseTree[]
+            games.push(gamesOnFile)
         }
     }
 
-    return games;
-};
+    return games
+}
 
